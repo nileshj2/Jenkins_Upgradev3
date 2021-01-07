c679d6a6eb763f3a54bbdb4a14dd4b27d1b53f0f
nileshj2 <38121791+nileshj2@users.noreply.github.com>
2021-01-07 23:18:52 +0530

    Update TestGreeter.java
    Added my name in comment

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "Devops, Thanks you making me Happy";
+    String someone = "Devops, Thanks you making Nilesh Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
